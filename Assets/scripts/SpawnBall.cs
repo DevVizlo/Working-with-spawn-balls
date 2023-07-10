18432d1f1227251e8e73caa63a177fbed4013592
Vizlo <109802629+DevVizlo@users.noreply.github.com>
2023-07-11 00:46:40 +0300

    Update SpawnBall.cs

--- Assets/scripts/SpawnBall.cs
+++ Assets/scripts/SpawnBall.cs
@@ -3,24 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Transform))]
 public class SpawnBall : MonoBehaviour
 {
     [SerializeField] private BallDelete _ballPrefab;
+    [SerializeField] private Transform _path;
 
+    private Transform[] _points;
     private float _timeSpawn = 2f;
-    private float _timer;
 
-    void Start()
+    private void Start()
     {
+        _points = new Transform[_path.childCount];
+
+        for (int i = 0; i < _path.childCount; i++)
+        {
+            _points[i] = _path.GetChild(i);
+        }
+
         StartCoroutine(BallSpawn());
-        _timer = _timeSpawn;
         Instantiate(_ballPrefab);
     }
 
     private IEnumerator BallSpawn()
     {
+        for (int i = 0; i < _points.Length; i++)
+        {
+            Instantiate(_ballPrefab, _points[i].position, Quaternion.identity);
             yield return new WaitForSeconds(_timeSpawn);
-            Instantiate(_ballPrefab, transform);
+        }
         
     }
 }
